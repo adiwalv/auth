463c14a3a7f972a8523d6445e19fec163596501f
Vikas Adiwal <vikas.adiwal@qio.io>
2020-09-13 23:59:38 +0530

    add DefaultConfig

--- src/main/java/com/adiwal/auth/AuthApplication.java
+++ src/main/java/com/adiwal/auth/AuthApplication.java
@@ -1,56 +1,13 @@
 package com.adiwal.auth;
 
-import com.adiwal.auth.domain.AuthClientDetails;
-import com.adiwal.auth.domain.User;
-import com.adiwal.auth.enums.Authorities;
-import com.adiwal.auth.repository.AuthClientRepository;
-import com.adiwal.auth.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @SpringBootApplication
-public class AuthApplication implements CommandLineRunner {
-
-	@Autowired
-	UserRepository userRepository;
-
-	@Autowired
-	AuthClientRepository authClientRepository;
-
-	@Lazy
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+public class AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		if (this.userRepository.findByUsername("vikas").isEmpty()) {
-			User user = new User();
-			Set<Authorities> authorities = new HashSet<>();
-			authorities.add(Authorities.ROLE_USER);
-			user.setUsername("vikas");
-			user.setActivated(true);
-			user.setAuthorities(authorities);
-			user.setPassword(passwordEncoder.encode("root123"));
-			this.userRepository.save(user);
-		}
-		if (this.authClientRepository.findByClientId("vikas").isEmpty()) {
-			AuthClientDetails browserClientDetails = new AuthClientDetails();
-			browserClientDetails.setClientId("vikas");
-			browserClientDetails.setClientSecret(passwordEncoder.encode("vikas"));
-			browserClientDetails.setScopes("ui");
-			browserClientDetails.setGrantTypes("refresh_token,password");
-			authClientRepository.save(browserClientDetails);
-		}
-	}
 }
---
+++ src/main/java/com/adiwal/auth/defaultsettings/DefaultConfig.java
@@ -0,0 +1,79 @@
+package com.adiwal.auth.defaultsettings;
+
+import com.adiwal.auth.domain.AuthClientDetails;
+import com.adiwal.auth.domain.User;
+import com.adiwal.auth.enums.Authorities;
+import com.adiwal.auth.repository.AuthClientRepository;
+import com.adiwal.auth.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
+@Service
+public class DefaultConfig {
+
+    @Value("${config.user.superUser}")
+    private String superUser;
+
+    @Value("${config.user.superUserPassword}")
+    private String superUserPassword;
+
+    @Value("${config.client.rootClient}")
+    private String rootClient;
+
+    @Value("${config.client.rootClientPassword}")
+    private String rootClientPassword;
+
+    @Value("${config.client.userClient}")
+    private String userClient;
+
+    @Value("${config.client.userClientPassword}")
+    private String userClientPassword;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    AuthClientRepository authClientRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostConstruct
+    public void init() {
+        if (this.userRepository.findByUsername(superUser).isEmpty()) {
+            User user = new User();
+            Set<Authorities> authorities = new HashSet<>();
+            authorities.add(Authorities.ROLE_USER);
+            user.setUsername(superUser);
+            user.setPassword(passwordEncoder.encode(superUserPassword));
+            user.setActivated(true);
+            user.setAuthorities(authorities);
+            this.userRepository.save(user);
+        }
+        if (this.authClientRepository.findByClientId(rootClient).isEmpty()) {
+            AuthClientDetails browserClientDetails = new AuthClientDetails();
+            browserClientDetails.setClientId(rootClient);
+            browserClientDetails.setClientSecret(passwordEncoder.encode(rootClientPassword));
+            browserClientDetails.setScopes("server");
+            browserClientDetails.setGrantTypes("refresh_token,password");
+            authClientRepository.save(browserClientDetails);
+        }
+        if (this.authClientRepository.findByClientId(userClient).isEmpty()) {
+            AuthClientDetails browserClientDetails = new AuthClientDetails();
+            browserClientDetails.setClientId(rootClient);
+            browserClientDetails.setClientSecret(passwordEncoder.encode(userClientPassword));
+            browserClientDetails.setScopes("ui");
+            browserClientDetails.setGrantTypes("refresh_token,password");
+            authClientRepository.save(browserClientDetails);
+        }
+    }
+
+}
