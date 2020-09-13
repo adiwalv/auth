d9034f7406e1ebe2ce44e76a0868e4bdd2ab42f9
Vikas Adiwal <vikas.adiwal@qio.io>
2020-09-13 22:32:50 +0530

    added refresh token

--- src/main/java/com/adiwal/auth/AuthApplication.java
+++ src/main/java/com/adiwal/auth/AuthApplication.java
@@ -1,5 +1,10 @@
 package com.adiwal.auth;
 
+import com.adiwal.auth.domain.AuthClientDetails;
+import com.adiwal.auth.domain.User;
+import com.adiwal.auth.enums.Authorities;
+import com.adiwal.auth.repository.AuthClientRepository;
+import com.adiwal.auth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,20 +12,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Arrays;
-
-import com.adiwal.auth.user.model.User;
-import com.adiwal.auth.user.repository.UserRepository;
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class AuthApplication implements CommandLineRunner {
+
 	@Autowired
-	private UserRepository userRepository;
+	UserRepository userRepository;
+
+	@Autowired
+	AuthClientRepository authClientRepository;
 
 	@Lazy
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
@@ -28,11 +34,23 @@ public class AuthApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		if (this.userRepository.findByUsername("vikas") == null) {
-			User user = new User("Vikas Adiwal", "vikas", passwordEncoder.encode("root123"), Arrays.asList("ADMIN"));
-
+		if (this.userRepository.findByUsername("vikas").isEmpty()) {
+			User user = new User();
+			Set<Authorities> authorities = new HashSet<>();
+			authorities.add(Authorities.ROLE_USER);
+			user.setUsername("vikas");
+			user.setActivated(true);
+			user.setAuthorities(authorities);
+			user.setPassword(passwordEncoder.encode("root123"));
 			this.userRepository.save(user);
 		}
+		if (this.authClientRepository.findByClientId("vikas").isEmpty()) {
+			AuthClientDetails browserClientDetails = new AuthClientDetails();
+			browserClientDetails.setClientId("vikas");
+			browserClientDetails.setClientSecret(passwordEncoder.encode("vikas"));
+			browserClientDetails.setScopes("ui");
+			browserClientDetails.setGrantTypes("refresh_token,password");
+			authClientRepository.save(browserClientDetails);
+		}
 	}
-
 }
---
+++ src/main/java/com/adiwal/auth/service/UserService.java
@@ -0,0 +1,7 @@
+package com.adiwal.auth.service;
+
+import com.adiwal.auth.domain.User;
+
+public interface UserService {
+    User create(User user);
+}
