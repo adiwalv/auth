6a6fe4533604b8a9b7bd90f904d24ce33ec22c50
Vikas Adiwal <vikas.adiwal@qio.io>
2020-09-13 23:37:21 +0530

    added role_admin

--- src/main/java/com/adiwal/auth/enums/Authorities.java
+++ src/main/java/com/adiwal/auth/enums/Authorities.java
@@ -4,7 +4,7 @@ import org.springframework.security.core.GrantedAuthority;
 
 public enum Authorities implements GrantedAuthority {
 
-    ROLE_USER;
+    ROLE_USER, ROLE_ADMIN;
 
     @Override
     public String getAuthority() {
